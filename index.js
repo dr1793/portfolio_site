880eb178b290cc267b83539a41da21a0447338bf
dr1793 <dr1793@nyu.edu>
2020-12-28 01:08:38 -0500

    Adding the about and resume sections

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const navbarMenu = document.querySelector("#nav-links");
 const home = document.querySelector("#home");
 const home_section = document.querySelector("#home-section");
 const resume = document.querySelector("#resume");
+const resume_section = document.querySelector("#resume-section");
 const about = document.querySelector("#about");
+const about_section = document.querySelector("#about-section");
 
 about.addEventListener("click", () => {
   home_section.classList.add("hidden");
+  resume_section.classList.add("hidden");
+  about_section.classList.remove("hidden");
 });
 
 resume.addEventListener("click", () => {
   home_section.classList.add("hidden");
+  resume_section.classList.remove("hidden");
+  about_section.classList.add("hidden");
 });
 
 home.addEventListener("click", () => {
   home_section.classList.remove("hidden");
+  resume_section.classList.add("hidden");
+  about_section.classList.add("hidden");
 });
 
 burgerIcon.addEventListener("click", () => {
