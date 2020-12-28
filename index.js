2c3d6833718a74773d13535355df623c1e45e6e0
dr1793 <dr1793@nyu.edu>
2020-12-28 00:25:37 -0500

    div fixes and crab

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#nav-links");
 
+const home = document.querySelector("#home");
+const home_section = document.querySelector("#home-section");
+const resume = document.querySelector("#resume");
+const about = document.querySelector("#about");
+
+about.addEventListener("click", () => {
+  home_section.classList.add("hidden");
+});
+
+resume.addEventListener("click", () => {
+  home_section.classList.add("hidden");
+});
+
+home.addEventListener("click", () => {
+  home_section.classList.remove("hidden");
+});
+
 burgerIcon.addEventListener("click", () => {
   navbarMenu.classList.toggle("is-active");
   burgerIcon.classList.toggle("is-active");
