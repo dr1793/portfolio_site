00808d7a61921ff9c3bee40c0c633c25ffd05bcd
dr1793 <dr1793@nyu.edu>
2020-12-28 22:23:42 -0500

    Welcome to Vue

---
+++ data/cardInfo.js
@@ -0,0 +1,34 @@
+export const cardInfo = [
+  {
+    cardTitle: "Basic Tableau Regression Tester",
+    cardImage: "../images/tableau_js.png",
+    imageLink:
+      "https://help.tableau.com/current/api/js_api/en-us/JavaScriptAPI/js_api.htm",
+    cardText:
+      "A lightweight webapp that leverages the Tableau JavaScript API to perform basic regression testing of Tableau reports published to Tableau server.",
+    readybutton: true,
+    readylink: "",
+    readytext: "Coming Soon",
+  },
+  {
+    cardTitle: 'The "Parties" Database',
+    cardImage: "../images/party_db.png",
+    imageLink: "",
+    cardText:
+      'Demo ETL and warehousing for a database of "parties"! Based on the database of a popular events management software.',
+    readybutton: true,
+    readylink: "",
+    readytext: "Coming Soon",
+  },
+  {
+    cardTitle: "Self-Hosted Searx Instance",
+    cardImage: "../images/Logo_searx_b copy.png",
+    imageLink:
+      "https://help.tableau.com/current/api/js_api/en-us/JavaScriptAPI/js_api.htm",
+    cardText:
+      "Searx is an open source internet search engine aggregator. I maintain a personal instance of searx on a DigitalOcean VPC.",
+    readybutton: false,
+    readylink: "",
+    readytext: "Try it out!",
+  },
+];
---
+++ myvue.js
@@ -0,0 +1,8 @@
+import { cardInfo } from "./data/cardInfo.js";
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    phew: "Welcome to Vue", //cardInfo[0]["cardTitle"],
+  },
+});
