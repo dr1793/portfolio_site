995e0601ec0d628547485a952292ea20cae6d069
dr1793 <dr1793@nyu.edu>
2020-12-28 20:19:59 -0500

    working crab

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const body = document.querySelector("#body");
 
 const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#nav-links");
+const caveBottom = document.querySelector("#cave2");
 
 const home = document.querySelector("#home");
 const home_section = document.querySelector("#home-section");
@@ -9,6 +10,14 @@ const resume = document.querySelector("#resume");
 const resume_section = document.querySelector("#resume-section");
 const about = document.querySelector("#about");
 const about_section = document.querySelector("#about-section");
+
+function toggleCave() {
+  if (navbarMenu.classList.contains("is-active")) {
+    caveBottom.style.display = "block";
+  } else {
+    caveBottom.style.display = "none";
+  }
+}
 
 about.addEventListener("click", () => {
   home_section.classList.add("hidden");
@@ -31,19 +40,23 @@ home.addEventListener("click", () => {
 burgerIcon.addEventListener("click", () => {
   navbarMenu.classList.toggle("is-active");
   burgerIcon.classList.toggle("is-active");
+  toggleCave();
 });
 
 home_section.addEventListener("click", () => {
   navbarMenu.classList.remove("is-active");
   burgerIcon.classList.remove("is-active");
+  toggleCave();
 });
 
 resume_section.addEventListener("click", () => {
   navbarMenu.classList.remove("is-active");
   burgerIcon.classList.remove("is-active");
+  toggleCave();
 });
 
 about_section.addEventListener("click", () => {
   navbarMenu.classList.remove("is-active");
   burgerIcon.classList.remove("is-active");
+  toggleCave();
 });
