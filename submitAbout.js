c84597dea4cf4fe1575bd75232044c81a23ab640
dr1793 <dr1793@nyu.edu>
2021-01-01 02:51:10 -0500

    done with the message submission form front-end, but now there's a bug in the icons

--- submitAbout.js
+++ submitAbout.js
@@ -1,11 +1,103 @@
 function submitMessageAbout() {
-  var name = document.getElementById("Name").value;
-  var email = document.getElementById("Email").value;
-  var subject = document.getElementById("Subject").value;
-  var message = document.getElementById("Message").value;
+  //setting the submit button to load
+  const button = document.getElementById("submitButton");
+  button.classList.add("is-loading");
+  var submission = {};
+  submission.Name = document
+    .getElementById("Name")
+    .value.replace(/[\r\n]+/gm, "");
+  submission.Email = document
+    .getElementById("Email")
+    .value.replace(/[\r\n]+/gm, "");
+  submission.Subject = document
+    .getElementById("Subject")
+    .value.replace(/[\r\n]+/gm, "");
+  submission.Message = document.getElementById("Message").value;
 
-  console.log(name);
-  console.log(email);
-  console.log(subject);
-  console.log(message);
+  //Removing any previous errors
+  for (arg in submission) {
+    document.getElementById(arg).classList.remove("is-danger");
+  }
+  document.getElementById("submissionErrorMessage").innerHTML = "";
+
+  //Running the validation methods
+  validation(submission, button);
+
+  //Send the data to the API
+  sendToAPI();
+
+  button.classList.remove("is-loading");
+  button.classList.add("is-success");
+  button.innerHTML = "Message Received";
+  button.setAttribute("disabled", "");
+}
+
+function validation(submission, button) {
+  //first check that the user entered anything
+  for (arg in submission) {
+    if (!submission[arg]) {
+      setdanger(arg, button);
+      if (arg == "Subject") {
+        pushErrorMessagetoUser(
+          `Please enter the ${arg.toLowerCase()} of your message.`
+        );
+      } else {
+        pushErrorMessagetoUser(`Please enter your ${arg.toLowerCase()}`);
+      }
+      throw new Error(`User did not enter a ${arg}`);
+    }
+  }
+
+  //then check if the user entered a valid email
+  validateEmail(submission.Email, button);
+
+  //Check that none of the fields are too long
+  if (submission.Name.length > 50) {
+    setdanger("Name", button);
+    pushErrorMessagetoUser(
+      "Error processing information in the name field. Please shorten it and try again."
+    );
+    throw new Error("Name too long.");
+  }
+  if (submission.Email.length > 320) {
+    setdanger("Email", button);
+    pushErrorMessagetoUser(
+      "Error processing information in the email field. Please shorten it and try again."
+    );
+    throw new Error("Email too long ");
+  }
+  if (submission.Subject.length > 200) {
+    setdanger("Subject", button);
+    pushErrorMessagetoUser(
+      "Error processing information in the subject field. Please shorten it and try again."
+    );
+    throw new Error("Email too long ");
+  }
+  if (submission.Message.length > 5000) {
+    setdanger("Message", button);
+    pushErrorMessagetoUser(
+      "Error processing information in the message field. Please shorten it and try again."
+    );
+    throw new Error("Email too long ");
+  }
+}
+
+function sendToAPI() {}
+
+function pushErrorMessagetoUser(message) {
+  document.getElementById("submissionErrorMessage").innerHTML = message;
+}
+
+function setdanger(arg, button) {
+  button.classList.remove("is-loading");
+  document.getElementById(arg).classList.add("is-danger");
+}
+
+function validateEmail(email, button) {
+  var emailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!email.match(emailformat)) {
+    setdanger("Email", button);
+    pushErrorMessagetoUser("Please enter a valid Email address.");
+    throw new Error("Invalid Email");
+  }
 }
